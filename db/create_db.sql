5282d00067f0de1fc45622ead217f14c6e7c7120
Wojciech Kuchta <klapi85@gmail.com>
2021-10-31 14:45:52 +0100

    Authors in create_db.sql script

--- db/create_db.sql
+++ db/create_db.sql
@@ -7,9 +7,29 @@ DROP TABLE IF EXISTS `Library`.`Books` ;
 CREATE TABLE IF NOT EXISTS `Library`.`Books` (
  `BookId` INT NOT NULL AUTO_INCREMENT,
  `Title` VARCHAR(255) NOT NULL,
- `Author` VARCHAR(255) NOT NULL,
+ `Author` UNT NOT NULL,
  `ISBN` VARCHAR(255) NOT NULL,
  PRIMARY KEY (`BookId`));
 
+DROP TABLE IF EXISTS `Library`.`Authors` ;
+
+CREATE TABLE IF NOT EXISTS `Library`.`Authors` (
+ `Author` INT NOT NULL AUTO_INCREMENT,
+ `FirstName` VARCHAR(255) NOT NULL,
+ `LastName` VARCHAR(255) NOT NULL,
+ `Nationality` VARCHAR(255) NOT NULL,
+ PRIMARY KEY (`AuthorId`));
+ 
+INSERT INTO `Library`.`Authors`
+VALUES (1, "Jarosław", "Grzędowicz", "polska");
+
+INSERT INTO `Library`.`Authors`
+VALUES (2, "J. R. R.", "Tolkien", "brytyjska");
+ 
 INSERT INTO `Library`.`Books`
-VALUES (1, "Pan Lodowego Ogrodu", "Jarosław Grzędowicz", "9232132322312");
+VALUES (1, "Pan Lodowego Ogrodu", 1, "9232132322312");
+
+INSERT INTO `Library`.`Books`
+VALUES (2, "Władca Pierścieni", 2, "9234432322987");
+
+
